385b2bc993305c860a6dc0d09e3df1bba69f32db
Dup4 <lyuzhi.pan@gmail.com>
2022-12-26 21:05:00 +0800

    refactor: use CUR_DIR

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
 export UNIT_TEST_TARGET=unittest-clang
 case $1 in
     --help)
@@ -18,8 +20,8 @@ esac
 BAZEL=bazel
 if ! type bazel >/dev/null 2>&1; then
 	wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/bazel-4.0.0-installer-linux-x86_64.sh; chmod +x bazel-4.0.0-installer-linux-x86_64.sh
-	./bazel-4.0.0-installer-linux-x86_64.sh --prefix=`pwd`
-	BAZEL=`pwd`/bin/bazel
+	./bazel-4.0.0-installer-linux-x86_64.sh --prefix="${CUR_DIR}"
+	BAZEL="${CUR_DIR}/bin/bazel"
 fi
 
 ${BAZEL} build :benchmark
