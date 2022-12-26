5f522e3081d69a51a63c7f4aa8617564a9b941b9
Dup4 <lyuzhi.pan@gmail.com>
2022-12-26 21:04:05 +0800

    fix: var name

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -10,14 +10,14 @@ case $1 in
         export UNIT_TEST_TARGET=unittest-gcc
         ;;
     -c)
-        export UNITTEST_TARGET=unittest-clang
+        export UNIT_TEST_TARGET=unittest-clang
         ;;
     *)
 esac
 
 BAZEL=bazel
 if ! type bazel >/dev/null 2>&1; then
-	wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/bazel-4.0.0-installer-linux-x86_64.sh; chmod +x bazel-4.0.0-installer-linux-x86_64.sh  
+	wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/bazel-4.0.0-installer-linux-x86_64.sh; chmod +x bazel-4.0.0-installer-linux-x86_64.sh
 	./bazel-4.0.0-installer-linux-x86_64.sh --prefix=`pwd`
 	BAZEL=`pwd`/bin/bazel
 fi
