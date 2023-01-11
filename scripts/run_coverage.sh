3963237c780db1bef6f314f9132678cdf92f3cd9
Dup4 <lyuzhi.pan@gmail.com>
2023-01-11 13:00:27 +0800

    chore: coverage ci

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 set -e
-apt update
-apt install unzip openjdk-11-jdk -y
+
+CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+TOP_DIR="${CUR_DIR}/../"
+
+# apt update
+# apt install unzip openjdk-11-jdk -y
 
 BAZEL=bazel
 if ! type bazel >/dev/null 2>&1; then
-	wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/bazel-4.0.0-installer-linux-x86_64.sh; chmod +x bazel-4.0.0-installer-linux-x86_64.sh  
+	wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/bazel-4.0.0-installer-linux-x86_64.sh; chmod +x bazel-4.0.0-installer-linux-x86_64.sh
 	./bazel-4.0.0-installer-linux-x86_64.sh --prefix=`pwd`
 	BAZEL=`pwd`/bin/bazel
 fi
+
 COPT=
 if [ $# -gt 0 ]; then
     COPT=$1
 fi
 
 $BAZEL coverage --combined_report=lcov --instrument_test_targets unittest-gcc-coverage
-mv bazel-out/_coverage/_coverage_report.dat ./cov.out
+mv "${TOP_DIR}"/bazel-out/_coverage/_coverage_report.dat "${TOP_DIR}"/coverage.dat
