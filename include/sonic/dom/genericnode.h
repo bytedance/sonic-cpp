3488eb90572d7900429af6ef848ab7ba1cde74be
liuq19 <liuqiang2016@gmail.com>
2023-02-28 20:02:22 +0800

    fix: fix rvalue for addmember

--- include/sonic/dom/genericnode.h
+++ include/sonic/dom/genericnode.h
@@ -773,16 +773,14 @@ class GenericNode {
 
   /**
    * @brief Add a new member for this object.
-   * @tparam ValueType the type of member's value
    * @param key new member's name, must be string
-   * @param value rvalue or lvalue reference for value, must be NodeType
+   * @param value rvalue of the added value node
    * @param copyKey copy the key's string when creating the key node if copyKey
    * is true.
    * @return NodeType& Reference of the added member.
    * @note value will be moved.
    */
-  template <typename ValueType>
-  MemberIterator AddMember(StringView key, ValueType&& value, alloc_type& alloc,
+  MemberIterator AddMember(StringView key, NodeType&& value, alloc_type& alloc,
                            bool copyKey = true) {
     sonic_assert(this->IsObject());
     return downCast()->addMemberImpl(key, value, alloc, copyKey);
